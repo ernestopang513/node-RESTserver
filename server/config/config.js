e8ccd89ec10836fd5a361fdba2fac1eb1235e0d9
Ernesto Pang <ernestopang513@hotmail.com>
2021-02-05 19:16:44 -0600

    Actuarlizacion con la variable de entorno en heroku

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe2';
 } else {
-    urlDB = 'mongodb+srv://ernesto:RasxKoT3Gnvy0TZR@cluster0.fcexc.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
